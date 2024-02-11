d721ecdf11052568d06dab4628d13198b16ed620
dev kartik <1kartikshettar@gmail.com>
2024-02-11 13:02:36 +0530

    added-JS:View project

--- src/Components/Banner/BannerSection.jsx
+++ src/Components/Banner/BannerSection.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-const ProjectShowcase = ({ banner }) => {
+const ProjectShowcase = ({ banner  }) => {
   return (
     <main
       className="max-w-4xl my-6 mx-2 sm:mx-auto bg-cover bg-center h-max text-zinc-800 py-24 px-10 rounded-lg opacity-80"
@@ -12,15 +12,21 @@ const ProjectShowcase = ({ banner }) => {
           Explore my diverse projects that showcase my skills in front-end, full-stack, and mobile development.
         </p>
 
-        <a
-          href="#"
+        <button
+        type='button'
+        onClick={() => {
+          const featuredSection = document.getElementById('featured-section');
+          if (featuredSection) {
+            featuredSection.scrollIntoView({ behavior: 'smooth' });
+          }
+        }}
           className="inline-flex items-center px-4 py-2 text-base font-medium rounded-md text-white bg-teal-700 hover:bg-teal-600 focus:ring-4 focus:outline-none focus:ring-teal-200"
         >
           View Projects
           <svg className="ml-2 w-4 h-4" viewBox="0 0 20 20" fill="white">
             <path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293z" clipRule="evenodd" />
           </svg>
-        </a>
+        </button>
       </section>
     </main>
   );
--- src/Components/FeaturedSection/Section.jsx
+++ src/Components/FeaturedSection/Section.jsx
@@ -5,9 +5,9 @@ import { memo } from "react";
 
  function Featured() {
   return (
-    <div className="max-w-4xl my-4 mx-auto p-4 lg:px-4">
-      <div className=" max-w-2xl py-4 sm:py-6 lg:max-w-none lg:py-8">
-        <h2 className="text-2xl font-bold text-gray-200">Featured Projects</h2>
+    <div className="max-w-4xl my-4 mx-auto p-4 lg:px-4" >
+      <div className=" max-w-2xl py-4 sm:py-6 lg:max-w-none lg:py-8" id="featured-section">
+        <h2 className="text-2xl font-bold text-gray-200" >Featured Projects</h2>
 
         <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
             {
