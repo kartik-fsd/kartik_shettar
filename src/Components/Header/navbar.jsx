22cc068a0c1ec2400d859eafb5adf8b83be11b3d
dev kartik <1kartikshettar@gmail.com>
2024-02-24 22:21:33 +0530

    fixed:mobile_menu_btn

--- src/Components/Header/navbar.jsx
+++ src/Components/Header/navbar.jsx
@@ -26,12 +26,15 @@ export default function Example() {
   });
 
   return (
-    <Disclosure as="nav" className="bg-gray-900 text-white shadow-sm shadow-neutral-950">
+    <Disclosure
+      as="nav"
+      className="bg-gray-900 text-white shadow-sm shadow-neutral-950"
+    >
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
                 <Disclosure.Button
                   className="p-2 text-gray-200 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                   aria-label={open ? "Close main menu" : "Open main menu"}
